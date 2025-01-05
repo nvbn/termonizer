daa715106ca1f54476116406ee3a6ab0ab5bde40
Vladimir Iakovlev <nvbn.rm@gmail.com>
2025-01-05 18:07:24 +0100

    add debug log support

--- cmd/termonizer/main.go
+++ cmd/termonizer/main.go
@@ -6,14 +6,27 @@ import (
 	"github.com/nvbn/termonizer/internal/repository"
 	"github.com/nvbn/termonizer/internal/storage"
 	"github.com/nvbn/termonizer/internal/ui"
+	"io"
+	"log"
 	"os"
 	"time"
 )
 
 var dbPath = flag.String("db", "${HOME}/.termonizer.db", "path to the database")
+var debug = flag.String("debug", "", "debug output path")
 
 func main() {
 	flag.Parse()
+
+	if *debug == "" {
+		log.SetOutput(io.Discard)
+	} else {
+		f, err := os.OpenFile(*debug, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			panic(err)
+		}
+		log.SetOutput(f)
+	}
 
 	ctx := context.Background()
 	goalsStorage, err := storage.NewSQLite(ctx, os.ExpandEnv(*dbPath))
